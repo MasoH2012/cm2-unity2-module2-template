d33b6da52c9814be34bf94f3471f2047ec4f8d34
emmanuelreyes28 <63267318+emmanuelreyes28@users.noreply.github.com>
2021-06-07 16:23:28 -0700

    First three exercises completed

--- Assets/Scripts/CubeController.cs
+++ Assets/Scripts/CubeController.cs
@@ -5,32 +5,27 @@ using TMPro;
 
 public class CubeController : MonoBehaviour
 {
-    private int currentNum;
-    public int score = 0;
     public TMP_Text scoreText;
-    public Vector3 position;
-    private float xPos;
-    private float yPos;
-    private float zPos;
+    //DECLARE VARIABLES HERE
+    
 
     // Start is called before the first frame update
     void Start()
     {
-        currentNum = 0;
-        transform.position = position;
-
+        //INITIALIZE CURRENTNUM HERE
     }
 
     // Update is called once per frame
     void Update()
     {
-        currentNum += 1;
-        print(currentNum);
+        //INCREMENT AND PRINT CURRENTNUM HERE 
 
-        if (Input.GetKeyDown(KeyCode.Mouse0))
-        {
-            score++;
-            scoreText.text = "Score: " + score.ToString();
-        }
+        //Uncomment the code below and complete 
+        //the if statement to check if Mouse0 is clicked 
+        // if ()
+        // {
+        //     score++;
+        //     scoreText.text = "Score: " + score.ToString();
+        // }
     }
 }
